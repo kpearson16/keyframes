dfcb062595970faae99f2bd55eabceae60a96c7b
kpearson16 <122332523+kpearson16@users.noreply.github.com>
2023-02-10 09:32:48 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const morePhotos = document.querySelector('.sec2');
+
+let morePhotosAppear = function(){
+morePhotos.style.display = "block"
+}
+
+
+
+
+
+const more = document.querySelector('.p2');
+
+let PhotosAppear = function(){
+more.style.display = "block"
+}
+
+const joke = document.querySelector('.sec3');
+
+let something = function(){
+joke.style.display = "flex"}
